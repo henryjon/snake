c762678c17f71f4afa95345eaa2c645f597d585a
Harry Giles <hjgiles001@gmail.com>
2019-10-12 01:03:39 +0100

    Added player and game classes

--- example.py
+++ example.py
@@ -1,6 +1,20 @@
 import snake
+import numpy as np
 
-board1 = snake.Board(10)
-board1.reset()
-board1.print()
-board1.step()
+
+def policy1(board):
+    return np.random.choice(["U", "R"])
+
+
+def policy2(board):
+    return np.random.choice(["U", "L"])
+
+
+n = 10
+
+player1 = snake.Player(n, policy1)
+player2 = snake.Player(n, policy2)
+
+game = snake.Game(n, player1, player2)
+for i in range(n + 2):
+    game.step()
--- snake.py
+++ snake.py
@@ -13,6 +13,9 @@ class Snake(object):
         self.player = player
         self.body = []
 
+    def __len__(self):
+        return len(self.body)
+
     def reset(self):
 
         if self.player == 1:
@@ -23,15 +26,43 @@ class Snake(object):
     def ixs(self):
         return [ix for ix, _direction in self.body]
 
-    def next_ixs(self):
+    def new_ix(self, ix, direction):
+        new_ix = {
+            "U": (lambda x: (x[0] - 1, x[1])),
+            "D": (lambda x: (x[0] + 1, x[1])),
+            "L": (lambda x: (x[0], x[1] - 1)),
+            "R": (lambda x: (x[0], x[1] + 1)),
+        }[direction](ix)
+        return new_ix
+
+    def new_head_ix(self):
+        ix, direction = self.body[0]
+        return self.new_ix(ix, direction)
+
+    def next_ixs(self, fruit_ix):
         """Returns the next set of ixs, will crash if not possible"""
-        # TODO
-        assert 1 == 0
+        if self.new_head_ix() == fruit_ix:
+            return [self.new_head_ix()] + self.ixs()
+        else:
+            return [self.new_head_ix()] + self.ixs()[:-1]
 
     def step_result(self):
         """Returns "Okay" or "Wall_collision"."""
-        # TODO
-        assert 1 == 0
+        new_head_ix = self.new_head_ix()
+        if new_head_ix[0] not in range(self.n) or new_head_ix[1] not in range(self.n):
+            return "Wall_collision"
+        else:
+            return "Okay"
+
+    def step(self, fruit_ix):
+        assert self.step_result() == "Okay"
+        new_head_ix = self.new_head_ix()
+        new_pair = (new_head_ix, self.body[0][1])
+
+        if new_head_ix == fruit_ix:
+            self.body = [new_pair] + self.body
+        else:
+            self.body = [new_pair] + self.body[:-1]
 
     def print(self):
         print("Snake: ", self.player)
@@ -93,7 +124,7 @@ class Board(object):
             assert step_result in ["Okay", "Wall_collision"]
 
             step_results[snake.player] = step_result
-            next_ixs[snake.player] = snake.next_ixs()
+            next_ixs[snake.player] = snake.next_ixs(self.fruit_ix)
 
         for player, other_player in zip([1, 2], [2, 1]):
             if step_results[player] == "Okay":
@@ -101,20 +132,26 @@ class Board(object):
                 other_ixs = next_ixs[other_player]
 
                 if head_ix in other_ixs:
-                    # This means a head on head collision will be a draw (both lose)
+                    # A head on head collision will be a draw (both lose)
                     step_results[player] = "Snake_collision"
 
         return step_results
 
     def step(self):
 
+        # Ensure that no snakes are going to collide with the wall or each other
         assert self.step_results() == {1: "Okay", 2: "Okay"}
 
+        fruit_eaten = False
         for snake in self.snakes:
-            snake.step()
+            old_len = len(snake)
+            snake.step(self.fruit_ix)
+            if len(snake) > old_len:
+                fruit_eaten = True
 
-        # TODO What about fruit
-        assert 1 == 0
+        if fruit_eaten:
+            self.fruit_ix = None
+            self.add_fruit()
 
     def image_str(self):
         image_str = np.full((self.n, self.n), " ")
@@ -141,3 +178,25 @@ class Board(object):
         print("Fruit: ", self.fruit_ix)
 
         print(self.image_str())
+
+
+class Player(object):
+    """A player on an n x n game of snake"""
+
+    def __init__(self, n, policy):
+        """[policy] is a function from a board state to a move in ["U", "D", "L", "R"]"""
+        self.n = n
+        self.policy = policy
+
+
+class Game(object):
+    def __init__(self, n, player1, player2):
+        self.n = n
+        self.player1 = player1
+        self.player2 = player2
+
+        self.board = Board(n)
+
+    def step(self):
+        # TODO
+        assert False
